79d981516cc4a2b2f6fe00225ad2aec12ff94427
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-11-09 12:03:42 -0600

    adding parse function

--- sarahindex.js
+++ sarahindex.js
@@ -4,53 +4,54 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2RyaW9zIiwiYSI6ImNrMm8xbzRpdjB5bG8zZ250N3lnc
 // testing change
 //set map variable 
 var map = new mapboxgl.Map({
-container: 'map', // container id
-style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-center: [-95.3698, 29.7604], // starting position [lng, lat]
-zoom: 9 // starting zoom
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: [-95.3698, 29.7604], // starting position [lng, lat]
+    zoom: 9 // starting zoom
 });
 
-map.on('load', function(){
+map.on('load', function () {
     map.addLayer({
         "id": "points",
         "type": "symbol",
         "source": {
-        "type": "geojson",
-        "data": {
+            "type": "geojson",
+            "data": {
                 "type": "FeatureCollection",
-                    "features": [{
+                "features": [{
                     "type": "Feature",
                     "geometry": {
-                    "type": "Point",
-                    "coordinates": [-77.03238901390978, 38.913188059745586]
+                        "type": "Point",
+                        "coordinates": [-77.03238901390978, 38.913188059745586]
                     },
                     "properties": {
-                    "title": "Mapbox DC",
-                    "icon": "monument"
+                        "title": "Mapbox DC",
+                        "icon": "monument",
+                        "description": "linktotickets"
                     }
-                    }, {
+                }, {
                     "type": "Feature",
                     "geometry": {
-                    "type": "Point",
-                    "coordinates": [-122.414, 37.776]
+                        "type": "Point",
+                        "coordinates": [-122.414, 37.776]
                     },
                     "properties": {
-                    "title": "Mapbox SF",
-                    "icon": "harbor"
+                        "title": "Mapbox SF",
+                        "icon": "harbor"
                     }
-                    }]
-                }
+                }]
+            }
         },
         "layout": {
-                "icon-image": "{icon}-15",
-                "text-field": "{title}",
-                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                "text-offset": [0, 0.6],
-                "text-anchor": "top"
-                 }
+            "icon-image": "{icon}-15",
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
     })
 })
 map.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     mapboxgl: mapboxgl
-    }));
+}));
