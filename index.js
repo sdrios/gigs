1ee705359d7735d1b1d59c77160399336383061a
Sarah <sarahd.rios@gmail.com>
2020-04-03 23:50:30 -0500

    mapbox - react structure

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+
 //set access token variable
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2RyaW9zIiwiYSI6ImNrMm8xbzRpdjB5bG8zZ250N3lncjdidXkifQ.2O_gYD_PFpq2XbsrHQtiyw';
 
+class App extends React.Component{
+constructor(props) {
+super(props);
+this.state = {
+lng: -97.6764,
+lat: 38.4067,
+zoom: 3.55
+
+};
+}
+
+componentDidMount(){
+
 //set map variable 
 var map = new mapboxgl.Map({
-container: 'map', // container id
+container: this.mapContainer, // container id
 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
 sprite: "mapbox://sprites/mapbox/bright-v15",
-attributionControl: false,
-center: [-97.6764, 38.4067], // starting position [lng, lat]
-zoom: 3.55 // starting zoom
+center: [this.state.lng, this.state.lat], // starting position [lng, lat]
+zoom: this.state.zoom // starting zoom
 });
+
+//add navigation controls to map
+map.addControl(new mapboxgl.NavigationControl());
+}
+
+
+render(){
+    return(
+        <div>
+            <div ref={el=>this.mapContainer = el} className="mapContainer"/>
+        </div>
+    )
+
+}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
 
 
 //display popup on-click event at the location of feature w/ description HTML from its feature properties
@@ -30,5 +65,3 @@ zoom: 3.55 // starting zoom
 
 
 
-//add navigation controls to map
-map.addControl(new mapboxgl.NavigationControl());
